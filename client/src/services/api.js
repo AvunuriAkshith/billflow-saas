51029c1d1a9e84001fd5f078337e164284b3f9ff
AunuriAkshith <avunuriakshith939@gmail.com>
2026-05-24 18:17:28 +0530

    Fixed production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'https://billflow-saas-rm1h.onrender.com/api',
+
+  baseURL:
+    'https://billflow-saas-rm1h.onrender.com/api'
+
 })
 
 export default API
