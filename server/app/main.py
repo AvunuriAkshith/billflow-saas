9477da304994322ea99256408df1fcef2c5a82bd
AunuriAkshith <avunuriakshith939@gmail.com>
2026-05-24 18:09:05 +0530

    Fixed CORS

--- server/app/main.py
+++ server/app/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://billflow-saas-ecru.vercel.app/"],
+    allow_origins=["https://billflow-saas-re73aoy01-avunuri-akshith-projects.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
